cd5d29e8716ef309d5c4c7586d0df3214c210936
Albin Varillas <81118732+Albin-Varillas@users.noreply.github.com>
2023-01-20 11:27:19 +0100

    added redux logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,19 @@
+import { useSelector } from "react-redux"
+
 export default function App() {
-    return <div className="bg-red-500">App</div>
+    const squares = useSelector((state) => state.reaction.squares)
+    console.log(squares)
+
+    const arr = Array(25).fill(0)
+
+    return (
+        <div className="bg-red-500">
+            App
+            <div className="bg-orange-500 grid grid-cols-5 gap-3 w-[500px] h-[500px]">
+                {squares.map((number, index) => (
+                    <div key={index} className="bg-blue-500"></div>
+                ))}
+            </div>
+        </div>
+    )
 }
